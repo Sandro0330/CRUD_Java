e7f783b84969821929db7f73724c9df341371aca
Sandro0330 <sandro030486@gmail.com>
2021-10-13 14:26:05 -0300

    Criando o banco e a tabela users

---
+++ sql/create-user-table.sql
@@ -0,0 +1,11 @@
+CREATE database 'jsp_crud';
+
+Use jsp_crud;
+
+create table users {
+	id int(3) NOT NULL AUTO_INCREMENT,
+	name varchar(120) NOT NULL,
+	email varchar(220) NOT NULL,
+	pais varchar(50),
+	PRIMARY KEY (id)
+};
